270ccac6e5f4f968f2112e0df75421ccf9842bd6
praveen <2311cs010457@mallareddyuniversity.ac.in>
2025-01-06 18:35:15 +0530

    first commit

---
+++ java_project/src/Day4_Conditionals.java
@@ -0,0 +1,7 @@
+public class Day4_Conditionals {
+    public static void main(String[] args) {
+        if(condition){
+            System.err.println("hello world");
+        }
+    }
+}
---
+++ java_project/src/Session_3.java
@@ -0,0 +1,15 @@
+public class Session_3 {
+    public static void main(String[] args) {
+        System.out.println(4==4);
+        System.out.println(4==6);
+        System.out.println(4<6);
+        String str="Hello";
+        int len = str.length();
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(len - 1));
+        System.out.println(str.substring(4));
+        System.out.println(str.substring(2,5));
+
+    }
+}
