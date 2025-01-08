4d418985c6b308a9d4422049b9639335a2b2b88d
mulagalapat <mulagalapati.praveen365@gmail.com>
2025-01-08 19:10:52 +0530

    Add files via upload

---
+++ java_project/src/Day__6.java
@@ -0,0 +1,25 @@
+
+
+public class Day__6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            
+            for (int j = 0; j <= i; j++) {
+                
+                System.out.print("a");
+                
+            }
+            System.out.println();
+        }
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        for(int i = 0; i < arr.length - 1;i++){
+            for(int j = i+1;j < arr.length; j++){
+                if(arr[i] + arr[j] == target) {
+                    ~
+                }
+            }
+        }
+
+    }
+}
